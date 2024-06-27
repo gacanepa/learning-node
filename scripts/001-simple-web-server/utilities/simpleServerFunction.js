ad226c0476abd00250a4eb13062af5785cd01db0
Gabriel Cánepa <gacanepa@gmail.com>
2024-06-27 15:58:31 -0300

    Use status codes and add HTML content to response

--- scripts/001-simple-web-server/utilities/simpleServerFunction.js
+++ scripts/001-simple-web-server/utilities/simpleServerFunction.js
@@ -1,7 +1,20 @@
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+
 const simpleServerFunction = (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    const userAgent = req.headers['user-agent'];
+    res.statusCode = StatusCodes.OK;
+    res.statusMessage = ReasonPhrases.OK;
+    // Tells the browser how to interpret the response body
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<!DOCTYPE html>');
+    res.write('<html>');
+    res.write('<head><title>Simple Web Server</title></head>');
+    res.write('<body>');
+    res.write('<h1>This is a simple HTML page</h1>');
+    res.write(`<p>You have browsed to ${req.url} using ${userAgent}.</p>`);
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
 };
 
 export default simpleServerFunction;
